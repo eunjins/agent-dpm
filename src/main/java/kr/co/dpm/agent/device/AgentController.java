70cc52ce4b25701d6177da0bcc53cf5ab6912483
ghdgh <ghdghd5560@gmail.com>
2021-11-26 22:33:48 +0900

    오류 수정

--- src/main/java/kr/co/dpm/agent/device/AgentController.java
+++ src/main/java/kr/co/dpm/agent/device/AgentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
